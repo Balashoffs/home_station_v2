2f18e3827b8c891b0bf205c0b0c9b5bd1a432030
alexbeard <balashov87aleksandr@gmail.com>
2020-02-29 19:52:21 +0700

    init progect

---
+++ home_station_hub/home_station_hub.ino
@@ -0,0 +1,22 @@
+/*
+ Name:		home_station_hub.ino
+ Created:	2/29/2020 7:34:21 PM
+ Author:	alexbeard
+*/
+
+// the setup function runs once when you press reset or power the board
+#include <RF24_config.h>
+#include <RF24.h>
+#include <printf.h>
+#include <nRF24L01.h>
+#include <LiquidCrystal.h>
+#include <DHT.h>
+#include <BMP085.h>
+void setup() {
+
+}
+
+// the loop function runs over and over again until power down or reset
+void loop() {
+  
+}
---
+++ home_station_v2_measure_module/home_station_v2_measure_module.ino
@@ -0,0 +1,15 @@
+/*
+ Name:		home_station_v2_measure_module.ino
+ Created:	2/29/2020 7:33:33 PM
+ Author:	alexbeard
+*/
+
+// the setup function runs once when you press reset or power the board
+void setup() {
+
+}
+
+// the loop function runs over and over again until power down or reset
+void loop() {
+  
+}
